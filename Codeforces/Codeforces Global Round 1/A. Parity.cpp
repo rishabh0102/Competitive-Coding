e1b45d2de13d2d705f1a54b247d50dc6bf8e18a9
Rishabh <f2014812@pilani.bits-pilani.ac.in>
2019-02-22 03:01:31 +0530

    first commit in sublime

--- Codeforces/Codeforces Global Round 1/A. Parity.cpp
+++ Codeforces/Codeforces Global Round 1/A. Parity.cpp
@@ -1,21 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define io ios_base::sync_with_stdio(false); cin.tie(0);
-#define mp make_pair
-#define pb push_back
 using namespace std;
-#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-
-void err(istream_iterator<string> it) {}
-template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
-	cerr << *it << " = " << a << endl;
-	err(++it, args...);
-}
-typedef long long int ll;
-typedef vector< int > vi;
-typedef vector< vi > vvi;
-typedef pair<int,int> ii;
 
 int main(int argc, char const *argv[])
 {
